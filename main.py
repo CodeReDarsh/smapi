f6c087c51adeda0ff0c39a44eabb8a5805f8a4ea
cdr <adarshcp2077@gmail.com>
2025-07-24 00:43:41 -0400

    completed Create and Read features for posts

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI, Response, status, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI() # FastAPI instance. Here "app" is the reference.
 
-
 # Schema is like a contract / blueprint between the frontend and backend (user and server) where the backend
 # expects the frontend to send data adhering to a strict format. Similar expectations are forced on the server for the frontend.
+
+# Posts schema. Validation done by Pydantic.
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: int | None = None 
+    rating: int | None = None # instead of int | None in python 3.9 and older you'd have to import Optional from the typing library
+                              # and use Optional[int] to do the same. Optional[X] is equivalent to X | None (or Union[X, None])
+
+# for now we're saving posts in memory instead of a database. Will come back to it later.
+my_posts = [
+    {
+        "title" : "title of post 1",
+        "content" : "content of post 1",
+        "published" : False,
+        "rating" : None,
+        "id" : 1,
+    },
+    {
+        "title" : "title of post 2",
+        "content" : "content of post 2",
+        "published" : False,
+        "rating" : None,
+        "id" : 2,
+    },
+]
+post_count = 2  # variable to assign post ids to new posts. Starts from 3 because we hard-coded the first two. Also acts as post
+                # counter.
 
 # path operation / route
 @app.get("/")   # function decorator is important to make the root function an actual path operation
@@ -33,20 +54,57 @@ def get_posts():
     return {"data" : "your posts here"}
 """
 
-# this get_posts finally executes.
+# Fetch latest post
+@app.get("/posts/latest")
+def latest_post() -> dict:
+    return {"detail" : my_posts[-1]}
+
+
+# Fetch a specific post by id
+@app.get("/posts/{id}")     # here the "id" field is the path parameter of the path operation. FastAPI automatically extracts this
+                            # path parameter and you can just past it as a parameter to your function.
+                            # NOTE PATH PARAMETERS ARE RETURNED AS STRINGS. However, if implicit conversion is possible you can 
+                            # convert the path paramenter into the required datatype by specifying the target type in the function
+                            # signature as a type hint/function decorator.
+def get_post(id : int, response : Response) -> dict:
+    post = next((post for post in my_posts if post["id"] == id), None)
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND    # By declaring a parameter of type `Response` in the path operation func
+        #                                                     # you can set the status code of the respone from that func.
+        #                                                     # Just import the Response class and the status modules to change the 
+        #                                                     # HTTP response codes. The status module contains macors/enums of all
+        #                                                     # commonly used response codes.
+        # return {"message" : f"post with id: {id} was not found :("}
+        # a better way of doing the same is to use a python exception called HTTPException from FastAPI
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found :(")
+        
+    return {"post detail" : post}
+
+
+# Fetches all posts from database
 @app.get("/posts")
-def get_posts():
-    return {"data" : "your posts are here! finally.."}
+def get_posts() -> dict:
+    return {"data" : my_posts}  # FastAPI automaticaly serializes the array `my_posts` into JSON format so that we can send it to 
+                                # our client.
 
-@app.post("/posts")
-def create_posts(post: Post): # type: ignore
-    print(post.title)
-    print(post.content)
-    print(post.published)
-    print(post.rating)
-    return post.dict()
 
-# each pydantic model has a .dict() function that returns a dictionary of the model's fields and values.
+# Creates new post with specified fields in database
+@app.post("/posts", status_code=status.HTTP_201_CREATED)    # as an extra feature, you can specify the response code of the path 
+                                                            # operation in the path's function decorator.
+def create_posts(post: Post) -> dict:
+    global post_count
+    post_count += 1
+    new_post = post.model_dump(mode="json")
+    new_post["id"] = post_count
+    my_posts.append(new_post)
+    
+    return {"data" : new_post}
+
+
+# each pydantic model has a .model_dump() function that returns a dictionary of the model's fields and values. You can additionally
+# set the mode parameter to "json" to make sure the output dict has only json-serializable objects as by default model_dump() can 
+# output non-json-serializable python objects. 
+# there is also a model_dump_json() function that's useful to dump stuff in json format instead of a dict.
 
 """
 CRUD for anything related to posting
@@ -65,7 +123,5 @@ D - Delete  (DELETE request)    /posts/:id
 
 standard convention is to name the urls in plural, so if you're creating a url for posting then, it would be /posts. Similarily 
 for a page for users would be /users not /user.
-
-
 
 """
